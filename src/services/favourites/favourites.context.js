a102ebc50bbeca4141be08e2355ca8c0ee489be0
siwachs <ssiwach.8888@gmail.com>
2024-02-04 16:49:29 +0530

    persistence storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,10 +1,31 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useMemo, useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+  const saveFavourites = async (items) => {
+    try {
+      const jsonItems = JSON.stringify(items);
+      await AsyncStorage.setItem("@favourites", jsonItems);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const items = await AsyncStorage.getItem("@favourites");
+      if (items !== null) {
+        setFavourites(JSON.parse(items));
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
   };
@@ -15,6 +36,14 @@ export const FavouritesContextProvider = ({ children }) => {
     );
     setFavourites(restaurants);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   const contextValue = useMemo(() => {
     return {
